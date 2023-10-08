9c435c91b080c6e920b1ca20d33fd35e7611baef
Dilum Sadeepa <51749100+dilumsadeepa@users.noreply.github.com>
2023-10-08 07:39:55 +0530

    add backend

---
+++ hms_backend/src/test/java/com/adms/hms/HmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adms.hms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
