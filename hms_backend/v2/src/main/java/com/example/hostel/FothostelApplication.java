203420cdd09db2fbad18f6f026aed82decac4065
Firstname Lastname <101855974+Avishkasj@users.noreply.github.com>
2023-10-27 14:33:09 +0530

    start v2

---
+++ hms_backend/v2/src/main/java/com/example/hostel/FothostelApplication.java
@@ -0,0 +1,13 @@
+package com.example.hostel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FothostelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FothostelApplication.class, args);
+	}
+
+}
---
+++ hms_backend/v2/src/test/java/com/example/hostel/FothostelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hostel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FothostelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
