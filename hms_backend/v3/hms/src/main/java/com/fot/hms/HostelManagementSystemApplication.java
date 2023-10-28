eb6cbe6d3b5f84660344a53f8db2328215c68b46
Dilum Sadeepa <51749100+dilumsadeepa@users.noreply.github.com>
2023-10-28 11:28:52 +0530

    add new

---
+++ hms_backend/v3/hms/src/main/java/com/fot/hms/HostelManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.fot.hms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostelManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostelManagementSystemApplication.class, args);
+	}
+
+}
---
+++ hms_backend/v3/hms/src/test/java/com/fot/hms/HostelManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fot.hms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostelManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
