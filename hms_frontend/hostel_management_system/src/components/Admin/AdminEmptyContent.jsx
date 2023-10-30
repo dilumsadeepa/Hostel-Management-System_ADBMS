e89b0c6e8ea7d097c929710619bffc9bdac6b99a
Lasith Randima <74246027+LasithRandima@users.noreply.github.com>
2023-10-30 19:35:59 +0530

    Cors Configuration Fixed

--- hms_frontend/hostel_management_system/src/components/Admin/AdminEmptyContent.jsx
+++ hms_frontend/hostel_management_system/src/components/Admin/AdminEmptyContent.jsx
@@ -1,10 +1,51 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import AdminNavbar from "./AdminNavbar";
 import AdminTopBar from "./AdminTopBar";
 import AdminFooter from "./AdminFooter";
 import DashboardTables from "./DashboardTables";
 
+import axios from 'axios';
+
+import 'jquery';
+import $ from 'jquery';
+import 'datatables.net-dt/css/jquery.dataTables.css';
+
 const AdminEmptyContent = () => {
+    const tableRef = useRef(null);
+
+  useEffect(() => {
+    // Function to fetch data from the API and initialize the DataTable
+    const fetchDataAndInitializeTable = async () => {
+      try {
+        // Fetch data from the API
+        const response = await axios.get('http://localhost:8080/res/all');
+        const data = response.data;
+        console.log(data);
+
+        // Initialize the DataTable
+        if (tableRef.current) {
+          $(tableRef.current).DataTable({
+            data: data,
+            columns: [
+              { data: 'resId' },
+              { data: 'roomNo' },
+              { data: 'resName' },
+              { data: 'installationDate' },
+              { data: 'lastMaintenanceDate' },
+              { data: 'status' },
+            ],
+          });
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    // Call the fetchDataAndInitializeTable function
+    fetchDataAndInitializeTable();
+  }, []);
+
+
   return (
     <div id="page-top">
       <div id="wrapper">
@@ -27,6 +68,20 @@ const AdminEmptyContent = () => {
 
 
               <h2>Your Resources Content Goes Here</h2>
+
+              <table ref={tableRef} className="display" style={{ width: '100%' }}>
+                <thead>
+                    <tr>
+                    <th>Resource ID</th>
+                    <th>Room No</th>
+                    <th>Resource Name</th>
+                    <th>Installation Date</th>
+                    <th>Last Maintenance Date</th>
+                    <th>Status</th>
+                    </tr>
+                </thead>
+                <tbody></tbody>
+                </table>
 
                 <DashboardTables />
 
