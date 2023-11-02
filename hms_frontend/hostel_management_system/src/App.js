9bac964e58eb5845e3a50c76c10487d4b33802d4
Lasith Randima <74246027+LasithRandima@users.noreply.github.com>
2023-11-02 11:35:33 +0530

    update navigation

--- hms_frontend/hostel_management_system/src/App.js
+++ hms_frontend/hostel_management_system/src/App.js
@@ -2,6 +2,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AdminHome from "./components/Admin/AdminHome";
 import AdminEmptyContent from "./components/Admin/AdminEmptyContent";
 import PageNotFound from "./components/Common/PageNotFound";
+<<<<<<< Updated upstream
+=======
+import Login from "./components/User/Login";
+import Register from "./components/User/Register";
+import Complaint from "./components/Admin/Complaint";
+>>>>>>> Stashed changes
 
 function App() {
   return (
@@ -11,6 +17,11 @@ function App() {
           <Route path="/" element={<AdminHome/>} />
           <Route path="/adminempty" element={<AdminEmptyContent/>} />
           <Route path="*" element={<PageNotFound/>} />
+<<<<<<< Updated upstream
+=======
+          <Route path="/complaints" element={<Complaint/>} />
+
+>>>>>>> Stashed changes
         </Routes>
       </BrowserRouter>
     </div>
