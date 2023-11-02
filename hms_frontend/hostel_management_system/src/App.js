b4cb410ce8150d5d2c08f43e5ff07bbee61ec8db
Madhuka Priyashan <111411737+MadhukaJS@users.noreply.github.com>
2023-11-02 15:37:29 +0530

    report dashboard routed

--- hms_frontend/hostel_management_system/src/App.js
+++ hms_frontend/hostel_management_system/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AdminHome from "./components/Admin/AdminHome";
 import AdminEmptyContent from "./components/Admin/AdminEmptyContent";
 import PageNotFound from "./components/Common/PageNotFound";
+import Report from "./components/Common/Report";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route path="/" element={<AdminHome/>} />
           <Route path="/adminempty" element={<AdminEmptyContent/>} />
           <Route path="*" element={<PageNotFound/>} />
+          <Route path="reports" element={<Report/>} />
         </Routes>
       </BrowserRouter>
     </div>
--- hms_frontend/hostel_management_system/src/components/Common/Report.jsx
+++ hms_frontend/hostel_management_system/src/components/Common/Report.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
 
 export default function Report() {
   return (
     <div>
-      
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">Asset Reports</h5>
+          <p className="card-text">common types of assets you might find in a hostel</p>
+          <a href="/add-users" className="btn ">
+            View Report
+          </a>
+        </div>
+      </div>
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">Maintenance Reports</h5>
+          <p className="card-text">broken assets you might find in a hostel</p>
+          <a href="/add-users" className="btn ">
+            View Report
+          </a>
+        </div>
+      </div>
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">Complaint</h5>
+          <p className="card-text">Conplaints about assets in the hostel</p>
+          <a href="/add-users" className="btn ">
+            View Report
+          </a>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
