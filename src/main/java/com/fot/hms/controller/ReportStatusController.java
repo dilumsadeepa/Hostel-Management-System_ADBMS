0c906780647429cbcb779f3d4da15690645d0ee3
Madhuka Priyashan <111411737+MadhukaJS@users.noreply.github.com>
2023-11-06 14:38:17 +0530

    API fixed.
    working

--- src/main/java/com/fot/hms/controller/ReportStatusController.java
+++ src/main/java/com/fot/hms/controller/ReportStatusController.java
@@ -1,8 +1,15 @@
 package com.fot.hms.controller;
 
+import com.fot.hms.model.ReportStatusEntity;
+import com.fot.hms.service.ReportStatusService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.List;
 
 @RestController
 @RequestMapping("/complaint-status")
@@ -10,5 +17,12 @@ public class ReportStatusController {
 
     @Autowired
 
-    private ComplaintDataService complaintDataService;
+    private ReportStatusService complaintDataService;
+
+    @GetMapping("report-status")
+    public List<ReportStatusEntity> getComplaintDataBetweenDates(
+            @RequestParam(name = "startDate") Date startDate,
+            @RequestParam(name = "endDate") Date endDate) {
+        return complaintDataService.GetComplaintMaintenance(startDate, endDate);
+    }
 }
